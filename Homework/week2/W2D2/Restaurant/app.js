dde0ba6880fe19ef04fa1b29d69467a4431aee87
Sujitha <vemulapallysujitha33@gmail.com>
2022-08-18 23:31:21 -0400

    commit

--- Homework/week2/W2D2/Restaurant/app.js
+++ Homework/week2/W2D2/Restaurant/app.js
@@ -1,21 +1,281 @@
-
-class menu{
-    constructor(item1, item2, item3, item4, item5, item6){
-        this.item1=item1
-        this.item2=item2
-        this.item3=item3
-        this.item4=item4
-        this.item5=item5
-        this.item6=item6
-
-    }
+let menu= {
+    Lunch: [
+        {
+            name: "Pancakes",
+            smallprice: "$6",
+            spicelevel: "Extra Pancake",
+            largeprice: "$7"
+        },
+        {
+            name: "Omelette",
+            smallprice: "$7",
+            spicelevel: "Extra Egg",
+            largeprice: "$8"
+        },
+        {
+            name: "Waffles",
+            smallprice: "$6",
+            spicelevel: "Lg",
+            largeprice: "$7"
+        },
+        {
+            name: "Home Fries",
+            smallprice: "$5",
+            spicelevel: "Lg",
+            largeprice: "$6"
+        },
+        {
+            name: "Hash Browns",
+            smallprice: "$5",
+            spicelevel: "Lg",
+            largeprice: "$6"
+        },
+        {
+            name: "Fruit Cup",
+            smallprice: "$4",
+            spicelevel: "Lg",
+            largeprice: "$5"
+        },
+    ],
+    Brunch: [
+        {
+            name: "sandwich",
+            smallprice: "$6",
+            spicelevel: "Extra Pancake",
+            largeprice: "$7"
+        },
+        {
+            name: "Omelette",
+            smallprice: "$7",
+            spicelevel: "Extra Egg",
+            largeprice: "$8"
+        },
+        {
+            name: "Waffles",
+            smallprice: "$6",
+            spicelevel: "Lg",
+            largeprice: "$7"
+        },
+        {
+            name: "Home Fries",
+            smallprice: "$5",
+            spicelevel: "Lg",
+            largeprice: "$6"
+        },
+        {
+            name: "Hash Browns",
+            smallprice: "$5",
+            spicelevel: "Lg",
+            largeprice: "$6"
+        },
+        {
+            name: "Fruit Cup",
+            smallprice: "$4",
+            spicelevel: "Lg",
+            largeprice: "$5"
+        },
+    ],
+    Dinner: [
+        {
+            name: "pizza",
+            smallprice: "$6",
+            spicelevel: "Extra Pancake",
+            largeprice: "$7"
+        },
+        {
+            name: "Omelette",
+            smallprice: "$7",
+            spicelevel: "Extra Egg",
+            largeprice: "$8"
+        },
+        {
+            name: "Waffles",
+            smallprice: "$6",
+            spicelevel: "Lg",
+            largeprice: "$7"
+        },
+        {
+            name: "Home Fries",
+            smallprice: "$5",
+            spicelevel: "Lg",
+            largeprice: "$6"
+        },
+        {
+            name: "Hash Browns",
+            smallprice: "$5",
+            spicelevel: "Lg",
+            largeprice: "$6"
+        },
+        {
+            name: "Fruit Cup",
+            smallprice: "$4",
+            spicelevel: "Lg",
+            largeprice: "$5"
+        },
+    ],
+    Desert: [
+        {
+            name: "Pancakes",
+            smallprice: "$6",
+            spicelevel: "Extra Pancake",
+            largeprice: "$7"
+        },
+        {
+            name: "Omelette",
+            smallprice: "$7",
+            spicelevel: "Extra Egg",
+            largeprice: "$8"
+        },
+        {
+            name: "Waffles",
+            smallprice: "$6",
+            spicelevel: "Lg",
+            largeprice: "$7"
+        },
+        {
+            name: "Home Fries",
+            smallprice: "$5",
+            spicelevel: "Lg",
+            largeprice: "$6"
+        },
+        {
+            name: "Hash Browns",
+            smallprice: "$5",
+            spicelevel: "Lg",
+            largeprice: "$6"
+        },
+        {
+            name: "Fruit Cup",
+            smallprice: "$4",
+            spicelevel: "Lg",
+            largeprice: "$5"
+        },
+    ],
+    Beverages: [
+        {
+            name: "Pancakes",
+            smallprice: "$6",
+            spicelevel: "Extra Pancake",
+            largeprice: "$7"
+        },
+        {
+            name: "Omelette",
+            smallprice: "$7",
+            spicelevel: "Extra Egg",
+            largeprice: "$8"
+        },
+        {
+            name: "Waffles",
+            smallprice: "$6",
+            spicelevel: "Lg",
+            largeprice: "$7"
+        },
+        {
+            name: "Home Fries",
+            smallprice: "$5",
+            spicelevel: "Lg",
+            largeprice: "$6"
+        },
+        {
+            name: "Hash Browns",
+            smallprice: "$5",
+            spicelevel: "Lg",
+            largeprice: "$6"
+        },
+        {
+            name: "Fruit Cup",
+            smallprice: "$4",
+            spicelevel: "Lg",
+            largeprice: "$5"
+        },
+    ],
+    Entrees: [
+        {
+            name: "Pancakes",
+            smallprice: "$6",
+            spicelevel: "Extra Pancake",
+            largeprice: "$7"
+        },
+        {
+            name: "Omelette",
+            smallprice: "$7",
+            spicelevel: "Extra Egg",
+            largeprice: "$8"
+        },
+        {
+            name: "Waffles",
+            smallprice: "$6",
+            spicelevel: "Lg",
+            largeprice: "$7"
+        },
+        {
+            name: "Home Fries",
+            smallprice: "$5",
+            spicelevel: "Lg",
+            largeprice: "$6"
+        },
+        {
+            name: "Hash Browns",
+            smallprice: "$5",
+            spicelevel: "Lg",
+            largeprice: "$6"
+        },
+        {
+            name: "Fruit Cup",
+            smallprice: "$4",
+            spicelevel: "Lg",
+            largeprice: "$5"
+        },
+    ],
+    Appetizers: [
+        {
+            name: "Pancakes",
+            smallprice: "$6",
+            spicelevel: "Extra Pancake",
+            largeprice: "$7"
+        },
+        {
+            name: "Omelette",
+            smallprice: "$7",
+            spicelevel: "Extra Egg",
+            largeprice: "$8"
+        },
+        {
+            name: "Waffles",
+            smallprice: "$6",
+            spicelevel: "Lg",
+            largeprice: "$7"
+        },
+        {
+            name: "Home Fries",
+            smallprice: "$5",
+            spicelevel: "Lg",
+            largeprice: "$6"
+        },
+        {
+            name: "Hash Browns",
+            smallprice: "$5",
+            spicelevel: "Lg",
+            largeprice: "$6"
+        },
+        {
+            name: "Fruit Cup",
+            smallprice: "$4",
+            spicelevel: "Lg",
+            largeprice: "$5"
+        }
+    ]
 }
 
-const Brunch = new menu("Biscuits", "Muffins","french toast", "pancakes", "scrambled egg", "")
-const Lunch = new menu("chicken salad", "egg roll","tacos", "chicken sandwich", "hamburger", "burger")
-const Dinner = new menu("steak", "")
-const Desert = new menu("ice cream", "cake", "brownie","")
-const Beverages = new menu()
-const Entrees = new menu("pizza", "")
-const Appetizers = new menu()
- 
+
+    let changeItems = (evt) => {
+        let menuitems = document.querySelectorAll(".menuitem")
+        for(let i = 0; i <menuitems.length; i++ ){
+            menuitems[i].innerHTML = `<ul>
+            <li class="item${i+1}">${menu[evt][i].name}</li>
+            <li class="item${i+1}">${menu[evt][i].smallprice}</li>
+            <li class="item${i+1}">${menu[evt][i].spicelevel}</li>
+            <li class="item${i+1}">${menu[evt][i].largeprice}</li>
+        </ul> `
+        }
+    }

0c1028fa86afc10683be929cadbba27048419ac5
Sujitha <vemulapallysujitha33@gmail.com>
2022-08-26 09:21:21 -0400

    created timmys story

--- Labs/week3/Day4/class.js
+++ Labs/week3/Day4/class.js
@@ -62,3 +62,33 @@ class Person {
   }
 
   //////////////////////////////
+
+
+  
+const timmy = new Person('Timmy')
+for(let i =0; i < 5; i++){
+  timmy.ageUp()
+}
+for(let i =0; i < 5; i++){
+  timmy.eat()
+}
+for(let i =0; i < 5; i++){
+  timmy.exercise()
+}
+for(let i =0; i < 9; i++){
+  timmy.ageUp()
+}
+const gus = new Hamster('Gus')
+gus.owner = 'Timmy'
+timmy.buyHamster(gus)
+
+for(let i =0; i < 15; i++){
+  timmy.ageUp()
+}
+timmy.eat()
+timmy.eat()
+timmy.exercise()
+timmy.exercise()
+
+
+////////////////////////////////////////

73cd156e940a950758414ef294e090347ba14c88
Sujitha <vemulapallysujitha33@gmail.com>
2022-09-22 16:21:04 -0400

    commit

---
+++ Express/Captains_Log/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+require('dotenv').config()
+
+
+mongoose.connect(process.env.MONGO_URI).then( () => {
+    console.log('connected to mongo');
+  });
+
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.use(express.urlencoded({extended:false}))
+app.use(methodOverride('_method'));
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+});
+
+
+app.get('/', (req, res)=>{
+    res.send('Welcome to the Captains Log!')
+})
+
+
+
+app.listen(3000, () => {
+    console.log('listening');
+});

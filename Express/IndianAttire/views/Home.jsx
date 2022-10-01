d4df81b4dae1b72576a82a005178c9973caf1960
Sujitha <vemulapallysujitha33@gmail.com>
2022-10-01 16:50:46 -0400

    commit ui

--- Express/IndianAttire/views/Home.jsx
+++ Express/IndianAttire/views/Home.jsx
@@ -6,37 +6,13 @@ class Home extends React.Component {
     return (
       <DefaultLayout>
         <div>
+        <header class="headersection">
+               <h3>Best Indian Clothing Online</h3>
+        </header>
 
+            
 
-            <header className="header">
-                <div className="header-wrapper">
-                    <div className="rleftpanel">left</div>
-                    <div className="title"><h1>Indian Attire</h1></div>
-                    <div className="rightpanel">right</div>
-                </div>
-            </header>
-
-            <div className="pageNavigationBar">
-                 <button> <a href="/coords"> Coords</a> </button> {""}
-                 <button> <a href="/dresses"> Dresses</a>  </button> {""}
-                 <button> <a href="/lehengas"> Lehengas</a>  </button> {""}
-                 <button> <a href="/sarees"> Sarees</a> </button> {""}
-                 <button> <a href="/suits"> Suits</a> </button> {""}
-            </div>
-
-
-            <footer className="footer">
-            <div class="footer-wrapper">
-                        <button>About Us</button>
-                        <a href="https://www.instagram.com" >
-                            <img src="https://www.pngkit.com/png/full/1-13187_instagram-logo-new-vector-eps-free-download-logo.png" className="instalogo"></img>
-                        </a>
-                        
-                        <a href="https://www.facebook.com">
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/1200px-2021_Facebook_icon.svg.png" className="facebooklogo"></img>
-                        </a> 
-            </div>
-        </footer>
+           
 
 
         </div>
--- Express/IndianAttire/views/layouts/Default.jsx
+++ Express/IndianAttire/views/layouts/Default.jsx
@@ -8,11 +8,41 @@ class DefaultLayout extends React.Component {
           <head>
             <title>{this.props.title}</title>
             <link rel="stylesheet" href="/css/app.css"></link>
+
+            <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'></link>
           </head>
           <body>
             <h1>{this.props.title}</h1>
+            
+            <nav className="navbar"> 
+                    <div className="nav">
+                        <img src="/images/logo.png" alt="logo" className="logo" />
+                        <div className="nav-items">
+                            <div className="search">
+                                <input type="text" class="search-box" placeholder="search products" />
+                                <button className="searchbtn">search</button>
+                            </div>
+                            <a href="#"><img src="/images/userimage.png"></img></a>
+                            <a href="#"><img src="/images/carticon.png"></img></a>
+                        </div>
+                    </div>
+
+                    <ul className="linksContainer">
+                          <li className="link-item"><a href="/" className="link"> Home</a></li>
+                          <li className="link-item"><a href="/coords" className="link"> Coords</a></li>
+                          <li className="link-item"><a href="/dresses" className="link"> Dresses</a></li>
+                          <li className="link-item"><a href="/lehengas" className="link"> Lehengas</a></li>
+                          <li className="link-item"><a href="/sarees" className="link"> Sarees</a> </li>
+                          <li className="link-item"><a href="/suits" className="link"> Suits</a></li>
+                    </ul>
+            </nav>
+
+            
+
+
             {this.props.children}
           </body>
+          <script src="/js/main.js"></script>
         </html>
       </>
     );

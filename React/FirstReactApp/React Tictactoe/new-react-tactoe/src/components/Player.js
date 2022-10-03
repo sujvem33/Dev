131f14c6c9126842870b4164ae8cf0939b593d44
Sujitha <vemulapallysujitha33@gmail.com>
2022-10-03 19:51:43 -0400

    commit reacttoe

--- React/FirstReactApp/React Tictactoe/new-react-tactoe/src/components/Player.js
+++ React/FirstReactApp/React Tictactoe/new-react-tactoe/src/components/Player.js
@@ -2,14 +2,11 @@ import React from 'react'
 
 function Player(props) {
   return (
-    <>
+    <div>
         <h2>Player {props.WhichPlayer}</h2>
         <h3>Wins:</h3>
         <p>Score: {props.Score}</p>
-    
-    
-    
-    </>
+    </div>
   )
 }
 

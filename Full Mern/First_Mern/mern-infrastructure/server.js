cc74aaa70ea7715ff6fd982d1b7db25e8750b837
Sujitha <vemulapallysujitha33@gmail.com>
2022-10-13 20:08:02 -0400

    commit DBchange

--- Full Mern/First_Mern/mern-infrastructure/server.js
+++ Full Mern/First_Mern/mern-infrastructure/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
-
 require('dotenv').config();
 require('./config/database');
 const port = process.env.PORT || 3001;
